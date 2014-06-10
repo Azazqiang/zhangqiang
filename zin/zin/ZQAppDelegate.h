ef17713d0b69e1895655a287ade74c1825627f3d
Azazqiang <zhang123789951>
2014-06-10 16:40:19 +0800

    第一天
    来看过了会撒谎

---
+++ zin/zin/ZQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZQAppDelegate.h
+//  zin
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 UICollectionView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zin/zin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zin
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 UICollectionView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZQAppDelegate class]));
+    }
+}
